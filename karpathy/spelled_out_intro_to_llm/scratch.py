cd9936f36276934e6df367a6aebd66fe1d7721e7
Mike Williams <mumakin@gmail.com>
2025-11-11 10:37:22 -0700

    finish probability based bigram model. about to move on to nn

--- karpathy/spelled_out_intro_to_llm/scratch.py
+++ karpathy/spelled_out_intro_to_llm/scratch.py
@@ -64,15 +64,26 @@ def learn_about_probability_and_sampling():
     print(itos[ix])
 
 
+# normalize the counts to get probabilities
+probs = counts.float()
+# props.sum(dim=1) gives us a column vector of the sum of each row, keepdim makes it a column vector again so we can do the division instead of a simple array
+probs /= probs.sum(dim=1, keepdim=True)
+# prove that each row sums to 1
+print(probs.sum(dim=1))
+
+
 # now lest generate some words
+# the point of this is that even though we are sampling the probability distribution of characters we still don't get great names
+# Mostly because we are only looking at one character back
 g = torch.Generator().manual_seed(2147483647)
 print("\ngenerate 20 names\n")
-for i in range(20):
+for i in range(5):
     ix = 0
     out = []
     while True:
-        p = counts[ix].float()
-        p = p / p.sum()
+        # p = counts[ix].float()
+        # p = p / p.sum()
+        p = probs[ix]
         # find the next letter by sampling from the distribution
         ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
         # if we found index 0 thats the . at the end of the word
@@ -81,5 +92,31 @@ for i in range(20):
         out.append(itos[ix])
     print("".join(out))
 
+print("\nlooking at the quality of the model")
+bigrams_considered = 0
+log_likelyhood = 0.0
+# calculate the quality of the probability model
+# recall that the random likelyhood is 1/27 for any given character so prop > 0.037 is good
+for w in words:
+    chs = ["."] + list(w) + ["."]
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        p = probs[ix1, ix2]
+        # using log to transform converst 0-1 probabilities to -infinity to 0 space. 0 would be a perfect prediction
+        # inverting the sign so that larger negative log likelihood is worse
+        # but 0 is still perfect
+        nll = -torch.log(p)
+        # print(f"{ch1}{ch2}: {p:.4f} {nll:.4f}")
+        bigrams_considered += 1
+        log_likelyhood += nll
+# average negative log likelyhood is a loss function
+# result was 2.4541
+print(
+    f"average negative log likelyhood per bigram: {log_likelyhood / bigrams_considered:.4f}"
+)
 
-# stop here for now https://www.youtube.com/watch?v=PaCmpygFfXo?t=36m0s
+# notice we could run the above on any given word
+# but words not in the training set, or more precicely words with letter pairs not in the training set will produce infinite loss, negative infinity
+# this sucks so people use model smoothing, which is adding a small count to every possible bigram so that no bigram has a 0 probability
+# this looks like prop = (count + 1).float() propb /= propb.sum(dim=1, keepdim=True)
