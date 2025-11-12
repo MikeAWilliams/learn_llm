7c2b62b71ea5d8e8f45b9a8f386cad77eec2ae70
Mike Williams <mumakin@gmail.com>
2025-11-12 16:29:38 -0700

    train it

--- karpathy/nnZeroToHero/02_mlp.py
+++ karpathy/nnZeroToHero/02_mlp.py
@@ -13,15 +13,18 @@ itos = {i: s for s, i in stoi.items()}
 # build the dataset
 block_size = 3  # context length: how many characters do we take to predict the output y
 X, Y = [], []
-for w in words[:5]:
-    print(w)
+for w in words:
+    # print(w)
     context = [0] * block_size
     for ch in w + ".":
         ix = stoi[ch]
         X.append(context)
         Y.append(ix)
-        print("".join(itos[i] for i in context), "->", itos[ix])
+        # print("".join(itos[i] for i in context), "->", itos[ix])
         context = context[1:] + [ix]  # crop and append
+
+X = torch.tensor(X)
+Y = torch.tensor(Y)
 
 g = torch.Generator().manual_seed(2147483647)
 # add an embedding layer C in the paper to embed each character into a smaller dimention space
@@ -33,21 +36,33 @@ C = torch.randn((27, 2), generator=g)
 W1 = torch.randn((block_size * 2, 100), generator=g)
 b1 = torch.randn(100, generator=g)
 # second layer of the network
-# from the 100 neurons in the hidden layer to 27 output classes (characters)
+# from the 101 neurons in the hidden layer to 27 output classes (characters)
 W2 = torch.randn((100, 27), generator=g)
 b2 = torch.randn(27, generator=g)
 parameters = [C, W1, b1, W2, b2]
 print(f"{sum(p.nelement() for p in parameters)} parameters")
 
-emb = C[X]
-# view reshapes the embeding tensor to allign with the first layer weight matrix W1. Equivelent to concatenating the 2D embeddings
-h = torch.tanh(emb.view(-1, 6) @ W1 + b1)
-logits = h @ W2 + b2
-# counts = logits.exp()
-# probs = counts / counts.sum(1, keepdim=True)
-# loss = -probs[torch.arange(32), Y].log().mean()
-# cross entropy is equivelent to the above 3 lines
-loss = F.cross_entropy(logits, torch.tensor(Y))
-print(loss)
+for p in parameters:
+    p.requires_grad = True
 
-# stop at https://youtu.be/TCH_1BHY58I?si=NWQTMs6NMwkUU2yu&t=2030
+for _ in range(1000):
+    # minibatch construct
+    ix = torch.randint(0, X.shape[0], (32,), generator=g)
+    emb = C[X[ix]]  # not X but a batch X[ix]
+    # view reshapes the embeding tensor to allign with the first layer weight matrix W1. Equivelent to concatenating the 2D embeddings
+    h = torch.tanh(emb.view(-1, 6) @ W1 + b1)
+    logits = h @ W2 + b2
+    # counts = logits.exp()
+    # probs = counts / counts.sum(1, keepdim=True)
+    # loss = -probs[torch.arange(32), Y].log().mean()
+    # cross entropy is equivelent to the above 3 lines
+    loss = F.cross_entropy(logits, torch.tensor(Y[ix]))
+    print(loss.item())
+    # backward pass
+    for p in parameters:
+        p.grad = None
+    loss.backward()
+    for p in parameters:
+        p.data += -0.1 * p.grad
+
+# continue at https://youtu.be/TCH_1BHY58I?si=eJ-tQeieGicjOeFk&t=2794
