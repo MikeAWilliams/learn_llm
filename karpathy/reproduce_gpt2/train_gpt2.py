53b6d3dbb79a9e4723fca6b3d2a21df0fc5d7908
Mike Williams <mumakin@gmail.com>
2025-12-28 15:16:48 -0700

    use fineweb

--- karpathy/reproduce_gpt2/train_gpt2.py
+++ karpathy/reproduce_gpt2/train_gpt2.py
@@ -2,6 +2,7 @@ import inspect
 import math
 import os
 import time
+from asyncio.events import AbstractEventLoopPolicy
 from dataclasses import dataclass
 from decimal import DefaultContext
 from operator import is_
@@ -271,26 +272,40 @@ class GPT(nn.Module):
 
 # -----------------------------------------------------------------------------
 
+import numpy as np
 import tiktoken
 
 
+def load_tokens(filename):
+    npt = np.load(filename)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+
 class DataLoaderLite:
-    def __init__(self, B, T, process_rank, num_processes):
+    def __init__(self, B, T, process_rank, num_processes, split):
         self.B = B
         self.T = T
         self.process_rank = process_rank
         self.num_processes = num_processes
 
-        # at init load tokens from disk and store them in memory
-        with open("input.txt", "r") as f:
-            text = f.read()
-        enc = tiktoken.get_encoding("gpt2")
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
+        assert split in {"train", "val"}
+        # tied to the structure in fineweb.py
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+
         if master_process:
-            print(f"loaded {len(self.tokens)} tokens")
+            print(f"found {len(self.shards)} shards for split {split}")
 
         # state
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
 
     def next_batch(self):
@@ -300,8 +315,10 @@ class DataLoaderLite:
         y = (buf[1:]).view(B, T)  # targets
         # advance the position in the tensor
         self.current_position += B * T * self.num_processes
-        # if loading the next batch would be out of bounds, reset
+        # if loading the next batch would be out of bounds, advance to the next shard
         if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
             self.current_position = self.B * self.T * self.process_rank
         return x, y
 
@@ -399,6 +416,7 @@ set_seed_on_device(device, 1337)
 # 79k tok/sec and 12,768 MiB MiB vram (B=16)
 
 total_batch_size = 524288  # 2**19, ~0.5M, in number of tokens
+# karpathy used B = 64
 B = 16  # micro batch size
 T = 1024  # sequence length
 assert total_batch_size % (B * T * ddp_world_size) == 0, (
@@ -409,7 +427,7 @@ if master_process:
     print(f"total desired batch size: {total_batch_size}")
     print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
 train_loader = DataLoaderLite(
-    B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size
+    B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="train"
 )
 
 
@@ -428,8 +446,8 @@ raw_model = model.module if ddp else model
 # implement variable learning rate
 max_lr = 6e-4
 min_lr = max_lr * 0.1
-warmup_steps = 10
-max_steps = 50
+warmup_steps = 715
+max_steps = 19073
 
 
 def get_lr(it):
@@ -476,13 +494,14 @@ for step in range(50):
     optimizer.step()
     sync_on_device(device)
     t1 = time.time()
-    dt = (t1 - t0) * 1000
-    tokens_per_second = (
+    dt = t1 - t0
+    tokens_processed = (
         train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
-    ) / (t1 - t0)
+    )
+    tokens_per_sec = tokens_processed / dt
     if master_process:
         print(
-            f"step {step}, loss {loss_accum.item():.6f}, | lr: {lr:.4e}, norm: {norm:.4f}, dt: {dt:.2f}ms, tok/sec: {tokens_per_second:0.2f}"
+            f"step {step:5d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt * 1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}"
         )
 
 if ddp:
